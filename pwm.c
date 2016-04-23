74768bc070745b8e0158c12ea7c25a0e5698a63c
Jiyong Youn <01@0101010101.com>
2016-04-24 03:51:29 +0900

    PWM fixed

--- pwm.c
+++ pwm.c
@@ -9,10 +9,12 @@ int main(int argc, char **argv) {
 	gpioSetPWMrange(20, 1000);
 	gpioSetPWMrange(21, 1000);
 
-	
-	int r, g, b;
-	scanf("%d%d%d", &r, &g, &b);
-	gpioPWM(16, r);
-	gpioPWM(20, g);
-	gpioPWM(21, b);
+	while(1) {
+		int r, g, b;
+		scanf("%d%d%d", &r, &g, &b);
+		gpioPWM(16, r);
+		gpioPWM(20, g);
+		gpioPWM(21, b);
+		puts("PWM set OK");
+	}
 }
